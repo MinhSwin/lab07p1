e447fc697637841ed076415d7643c3117a74310c
MinhSwin <106511074@student.swin.edu.au>
2026-04-23 14:52:11 +1000

    Arrays and shi

---
+++ daysarray.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DaysArray</title>
+</head>
+<body>
+    <h1>PHP Variables, Arrays and Operators</h1>
+    <?php
+    $days = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    echo "<p>The days of the week in English are: </p>";
+    //foreach ($days as $day) {
+        //echo "<p>$day</p>";
+    //}
+    echo "<p>" .implode(", " , $days)."</p>";
+    $days = array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Saturday");
+
+    echo "<p>The days of the week in French are: </p>";
+    echo "<p>" .implode(", " , $days)."</p>";
+    ?>
+</body>
+</html>
